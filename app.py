529a8fae636fd8d8dfc2258116c41a6bc3f4b233
Ayush <ayushsh285@gmail.com>
2025-01-12 16:49:56 -0500

    fix spelling

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def main():
 
 
                 df = df_filtered
-                df['Referer'] = 'Real Intent'
+                df['Referrer'] = 'Real Intent'
                 df['Source'] = 'Real Intent'
                 
                 if hashtag:
